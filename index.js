faf5aba42777ed50a3c3a2a04622ff12e7ff470f
Sterlyng Powell <sterlyng.powell@gmail.com>
2019-02-27 10:03:34 -0800

    Update index.js
    Added ID stuff

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 var enterButton = document.getElementById('submit');
 var userName = document.getElementById('user-name');
-var playButton = document.getElementsByTagName('button');
-
-document.querySelectorAll("color").addEventListener("click", function() {
-  // var buttonInnerHTML = this.innerHTML;
-  makeSound(event.playButton);
-  // makeSound(event.playButton);
-});
+var playButton = document.querySelectorAll("color");
+var clickedId = document.querySelectorAll("id"); //ID
+// For the above event, I have to get the browser to
+//figure out what ID was clicked in order for it to play.
 
 
-function makeSound(playButton) {
+  playButton.addEventListener("click", function() {
 
-  switch (playButton) {
+    makeSound();
+    // Messups
+    // var buttonInnerHTML = this.innerHTML;
+    // makeSound(event.playButtons);
+    // makeSound(event.playButton);
+  });
+
+
+
+function makeSound(clickedId) {
+
+  switch (clickedId) {
     case "red":
       var audio1 = new Audio("audio/unlock_1.wav");
+      alert("Red is Pressed");
       audio1.play();
       break;
 
     case "blue":
       var audio2 = new Audio("audio/unlock_2.wav");
+      alert("Blue is Pressed");
       audio2.play();
       break;
 
     case "green":
       var audio3 = new Audio("audio/unlock_3.wav");
+      alert("Green is Pressed");
       audio3.play();
       break;
 
     case "yellow":
       var audio4 = new Audio("audio/unlock_4.wav");
+      alert("Yellow is Pressed");
       audio4.play();
       break;
     default:
-      console.log();
+      console.log("Something was Pressed!");
   }
 }
-//Break??-------
+
+
+
+
+//-------More Messups-------
 
 
 // function makeSound(playButton) {
